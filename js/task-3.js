f1fb43c0b5c6135ffe562a457edd061c61df9f87
Gaanj <Dmytrobozescyl@gmail.com>
2023-10-27 23:07:26 +0300

    complete task 1-3 and create file for task-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const a = Number.parseFloat(content);
-    const b = Number.parseFloat(padding);
-    const c = Number.parseFloat(border);
-    return a + b * 2 + c * 2;
+function checkForSpam(message) {
+    let a = message.slice();
+    let b = a.toLowerCase();
+    if (b.includes('spam') || b.includes('sale')){
+return true;
+    }
+    else {
+        return false;
+    }
 }
-
-getElementWidth("50px", "8px", "4px"); // 74
-getElementWidth("60px", "12px", "8.5px"); // 101
-getElementWidth("200px", "0px", "0px"); // 200
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
